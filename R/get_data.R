58c32de3dde2ecd43a1e8af8a80d128bb6588035
ledell <erin@h2o.ai>
2016-04-01 15:35:25 -0700

    Restricted name input to list of data names

--- R/get_data.R
+++ R/get_data.R
@@ -6,10 +6,12 @@
 #' @examples
 #' data <- get_data(name = "supervia")
 #'
-get_data <- function(name){
-    names_data <- c("pontosculturais", "metro", "brt", "supervia")
+get_data <- function(name = c("pontosculturais", "metro", "brt", "supervia")) {
+  
+    name <- match.arg(name)
     url <- "http://dadosabertos.rio.rj.gov.br/"
     api_path <- "/api/v1/rest/"
+    names_data <- c("pontosculturais", "metro", "brt", "supervia")
     names_place <- c("PontosCulturais", "Estacoes", "EstacoesTranscarioca", "estacoes")
     tokens <- c("0DEF20BC-FCC3-316D-2B094AB44B2C4F76", "40A22Af7-A9C6-86D6-E211Aba64Bf73830",
                 "7A32FDD1-CF97-4B1D-4BEE71037BAAEAF2", "414353CA-D860-271B-3972665CCA1C05EE")
